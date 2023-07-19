38ad1d25dbe71ca534f1bd8b6abf90b60db57ac2
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-07-20 01:20:51 +0300

    Simple first page of app. Need to add cardview of rentals.

---
+++ AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/MainPageActivity.java
@@ -0,0 +1,66 @@
+package com.example.fakebnb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button chatButton = findViewById(R.id.chatButton);
+        Button profileButton = findViewById(R.id.profileButton);
+        Button roleButton = findViewById(R.id.roleButton);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed CHAT BUTTON", Toast.LENGTH_SHORT).show();
+//                Intent chat_intent = new Intent(getApplicationContext(), ChatActivity.class);
+//                startActivity(chat_intent);
+
+                // TODO: ONLY FOR TESTING PURPOSES. NEEDS TO BE REMOVED
+
+//                go to reservation done page
+//                Intent reservation_done_intent = new Intent(getApplicationContext(), ReservationDoneActivity.class);
+//                startActivity(reservation_done_intent);
+
+//                go to write review page
+//                Intent write_review_intent = new Intent(getApplicationContext(), WriteReviewPage.class);
+//                startActivity(write_review_intent);
+
+//                go to add new place page
+//                Intent add_new_place_intent = new Intent(getApplicationContext(), AddNewPlaceActivity.class);
+//                startActivity(add_new_place_intent);
+
+//                go to rent room page
+                Intent rent_room_intent = new Intent(getApplicationContext(), RentRoomPage.class);
+                startActivity(rent_room_intent);
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed PROFILE BUTTON", Toast.LENGTH_SHORT).show();
+                Intent profile_intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(profile_intent);
+            }
+        });
+
+        roleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed ROLE BUTTON", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
