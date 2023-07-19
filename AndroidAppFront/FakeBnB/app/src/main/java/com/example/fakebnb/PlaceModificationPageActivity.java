15d3736802183901c26741e62269464c7005beee
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-07-20 01:23:08 +0300

    Modification page activity.
    Not implemented yet

---
+++ AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/PlaceModificationPageActivity.java
@@ -0,0 +1,48 @@
+package com.example.fakebnb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlaceModificationPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_modification);
+
+        Button chatButton = findViewById(R.id.chatButton);
+        Button profileButton = findViewById(R.id.profileButton);
+        Button roleButton = findViewById(R.id.roleButton);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed CHAT BUTTON", Toast.LENGTH_SHORT).show();
+                Intent chat_intent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(chat_intent);
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed PROFILE BUTTON", Toast.LENGTH_SHORT).show();
+                Intent profile_intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(profile_intent);
+            }
+        });
+
+        roleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Pressed ROLE BUTTON", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
