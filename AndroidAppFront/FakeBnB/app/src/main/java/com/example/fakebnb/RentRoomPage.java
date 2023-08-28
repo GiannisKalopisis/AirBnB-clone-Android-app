d6b0aac61820de007d4f7153d07b121a8143fd89
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-08-28 14:50:53 +0300

    Added booking code. Need to implement all the API calls. NOT WORKING

--- AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/RentRoomPage.java
+++ AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/RentRoomPage.java
@@ -27,6 +27,12 @@ import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.fakebnb.adapter.RulesAdapter;
 import com.example.fakebnb.enums.RoleName;
 import com.example.fakebnb.model.RentRoomModel;
+import com.example.fakebnb.model.request.BookingRequest;
+import com.example.fakebnb.model.response.ApartmentResponse;
+import com.example.fakebnb.model.response.BookingResponse;
+import com.example.fakebnb.rest.ApartmentAPI;
+import com.example.fakebnb.rest.BookingAPI;
+import com.example.fakebnb.rest.RestClient;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -43,6 +49,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import retrofit2.Callback;
+
 
 public class RentRoomPage extends AppCompatActivity {
 
@@ -52,6 +60,8 @@ public class RentRoomPage extends AppCompatActivity {
     private Long userId;
     private String jwtToken;
     private Set<RoleName> roles;
+    private Long rentalId;
+    private ApartmentResponse.ApartmentData apartmentData;
 
     private TextView rentRoomPersonsValue, rentRoomBedsValue, rentRoomBathroomsValue,
             rentRoomBedroomsValue, rentRoomPriceValue, rentRoomExtraPriceValue, rentRoomFinalPriceValue,
@@ -70,6 +80,8 @@ public class RentRoomPage extends AppCompatActivity {
     private boolean userStayedAtRental = false;
     private TextView rentRoomReviewTitle;
 
+    private ArrayList<SlideModel> slideModels;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -80,6 +92,7 @@ public class RentRoomPage extends AppCompatActivity {
         if (intent != null) {
             userId = intent.getSerializableExtra("user_id", Long.class);
             jwtToken = intent.getSerializableExtra("user_jwt", String.class);
+            rentalId = intent.getSerializableExtra("rental_id", Long.class);
             ArrayList<String> roleList = intent.getStringArrayListExtra("user_roles");
             if (roleList != null) {
                 roles = new HashSet<>();
@@ -89,44 +102,137 @@ public class RentRoomPage extends AppCompatActivity {
             }
         }
 
-        checkGoogleAPIAvailability();
+        /**
+         * Do all the API calls here:
+         * 1. Get the rental info - Failure is NOT_OK -> go back to main page
+         * 2. Get the rental images - Failure is OK
+         * 3. Get the host image - Failure is OK
+         */
 
-        // Check for location permissions and request if not granted
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-        } else {
-            // Proceed with initializing the MapView and displaying the map
-            initView();
-            bottomBarClickListeners();
 
-            createSlider();
-            info = new RentRoomModel(4, 2, 2, 100, 15,"Entire House",
-                    "This is a descriptionwerg wgwe werg wergwer gwergwer gwerg werg wwerg werfgwefg wdfgsdfgwertg wergswdfg sdfg sdfg we ",
-                    "These are the rules. These are the rules1. These are the rules2. These are the rules3. These are the rules4. These are the rules5.",
-                    "Sperchiou 70, Peristeri 121 37", "These are the amenities. These are the amenities1. These are the amenities2. These are the amenities3. These are the amenities4.",
-                    "photo_path.png", "Sakis Karpas", true);
-            rentRoomMapView.onCreate(savedInstanceState);
-            rentRoomMapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(@NonNull GoogleMap googleMap) {
-                    // Handle the map ready event here.
-                    // Now you can use the "googleMap" instance to work with the map.
-                    // Add the logic to display the address on the map here.
-                    isMapReady = true; // Mark the map as ready
-                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                    googleMap.getUiSettings().setZoomControlsEnabled(true);
-                    // Check if an address is available and show it on the map
-                    if (info != null) {
-                        showAddressOnMap(googleMap, info.getAddress());
-                    }
-                }
-            });
-            getDataFromDatabase();
-            buttonClickListener();
-        }
+        /**
+         * Get RENTAL-INFO. In case of success, do the rest of the work:
+         * a. initialize the map and the rest logic
+         * b. render data
+         * c. get the rental images - failure is OK
+         * d. get the host image - failure is OK
+         */
+
+        RestClient restClient = new RestClient(jwtToken);
+        ApartmentAPI apartmentAPI = restClient.getClient().create(ApartmentAPI.class);
+
+        fetchApartmentInfo(rentalId, apartmentAPI, savedInstanceState);
+
+        info = new RentRoomModel(4, 2, 2, 100, 15,"Entire House",
+                "This is a descriptionwerg wgwe werg wergwer gwergwer gwerg werg wwerg werfgwefg wdfgsdfgwertg wergswdfg sdfg sdfg we ",
+                "These are the rules. These are the rules1. These are the rules2. These are the rules3. These are the rules4. These are the rules5.",
+                "Sperchiou 70, Peristeri 121 37", "These are the amenities. These are the amenities1. These are the amenities2. These are the amenities3. These are the amenities4.",
+                "photo_path.png", "Sakis Karpas", true);
+
 
         Toast.makeText(this, TAG + " ID: " + getIntent().getIntExtra("rental_id", 0) + 1, Toast.LENGTH_SHORT).show();
     }
+
+    /**
+     * API calls methods
+     */
+
+    private void fetchApartmentInfo(long rentalId, ApartmentAPI apartmentAPI, @Nullable Bundle savedInstanceState) {
+        apartmentAPI.getApartmentInfo(rentalId)
+                .enqueue(new Callback<ApartmentResponse>() {
+                    @Override
+                    public void onResponse(@NonNull retrofit2.Call<ApartmentResponse> call, @NonNull retrofit2.Response<ApartmentResponse> response) {
+                        handleResponse(response, apartmentAPI, savedInstanceState);
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull retrofit2.Call<ApartmentResponse> call, @NonNull Throwable t) {
+                        showToast("Failed to connect to server");
+                        finish();
+                    }
+                });
+    }
+
+    private void handleResponse(retrofit2.Response<ApartmentResponse> response, ApartmentAPI apartmentAPI, @Nullable Bundle savedInstanceState) {
+        if (response.isSuccessful()) {
+            if (response.body() != null) {
+                apartmentData = response.body().getObject();
+
+                checkGoogleAPIAvailability();
+
+                // Check for location permissions and request if not granted
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+                } else {
+                    // Proceed with initializing the MapView and displaying the map
+                    initView();
+                    bottomBarClickListeners();
+
+                    rentRoomMapView.onCreate(savedInstanceState);
+                    rentRoomMapView.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            isMapReady = true; // Mark the map as ready
+                            googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                            googleMap.getUiSettings().setZoomControlsEnabled(true);
+                            // Check if an address is available and show it on the map
+                            if (info != null) {
+                                showAddressOnMap(googleMap, info.getAddress());
+                            }
+                        }
+                    });
+
+                    renderFetchedData();
+                    buttonClickListener();
+                    createSlider();
+                    fetchApartmentImages(rentalId, apartmentAPI);
+                    fetchHostImage(apartmentData.getId(), apartmentAPI);
+                }
+            } else {
+                showToast("Could not get rental info");
+                finish();
+            }
+        } else {
+            showToast("Could not get rental info");
+            finish();
+        }
+    }
+
+    private void fetchApartmentImages(long rentalId, ApartmentAPI apartmentAPI) {
+        /*
+        apartmentAPI.callOfImages()...
+        if (success) {
+            slideModels.addAllImages();
+        }
+        else {
+            showToast("Could not get rental images");
+            continue;
+        }
+         */
+    }
+
+    private void fetchHostImage(long hostId, ApartmentAPI apartmentAPI) {
+        /*
+        // get host id of the apartment
+        apartmentAPI.callOfHostImage()...
+        if (success) {
+            slideModels.addHostImage();
+        }
+        else {
+            showToast("Could not get host image");
+            continue;
+        }
+         */
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(RentRoomPage.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * Google Maps methods
+     */
 
     private void checkGoogleAPIAvailability() {
         // Check for Google Play Services availability
@@ -192,7 +298,10 @@ public class RentRoomPage extends AppCompatActivity {
         return locationLatLng;
     }
 
-    private void getDataFromDatabase() {
+    /**
+     * Data manipulation methods
+     */
+    private void renderFetchedData() {
         // get the data
         // parse the data
 
@@ -284,7 +393,7 @@ public class RentRoomPage extends AppCompatActivity {
     private void createSlider() {
         // get images from database
         ImageSlider imageSlider = findViewById(R.id.cardImageSlider);
-        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels = new ArrayList<>();
 
         slideModels.add(new SlideModel(R.drawable.login_image, ScaleTypes.FIT));
         slideModels.add(new SlideModel(R.drawable.register_image, ScaleTypes.FIT));
@@ -395,8 +504,19 @@ public class RentRoomPage extends AppCompatActivity {
         seeHostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: get the host id from the API
+                long hostId = 1L;
+
                 Toast.makeText(view.getContext(), "Pressed SEE HOST BUTTON", Toast.LENGTH_SHORT).show();
                 Intent see_host_intent = new Intent(getApplicationContext(), HostReviewPageActivity.class);
+                see_host_intent.putExtra("user_id", userId);
+                see_host_intent.putExtra("user_jwt", jwtToken);
+                ArrayList<String> roleList = new ArrayList<>();
+                for (RoleName role : roles) {
+                    roleList.add(role.toString());
+                }
+                see_host_intent.putExtra("user_roles", roleList);
+                see_host_intent.putExtra("host_id", hostId);
                 startActivity(see_host_intent);
             }
         });
@@ -404,8 +524,19 @@ public class RentRoomPage extends AppCompatActivity {
         contactHostButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: get the host id from the API
+                long hostId = 2L;
+
                 Toast.makeText(view.getContext(), "Pressed CONTACT HOST BUTTON", Toast.LENGTH_SHORT).show();
-                Intent contact_host_intent = new Intent(getApplicationContext(), IndividualChatPage.class);
+                Intent contact_host_intent = new Intent(getApplicationContext(), IndividualChatActivity.class);
+                contact_host_intent.putExtra("user_id", userId);
+                contact_host_intent.putExtra("user_jwt", jwtToken);
+                ArrayList<String> roleList = new ArrayList<>();
+                for (RoleName role : roles) {
+                    roleList.add(role.toString());
+                }
+                contact_host_intent.putExtra("user_roles", roleList);
+                contact_host_intent.putExtra("other_user_id", hostId);
                 startActivity(contact_host_intent);
             }
         });
@@ -414,18 +545,67 @@ public class RentRoomPage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "Pressed MAKE RESERVATION BUTTON", Toast.LENGTH_SHORT).show();
-                Intent make_reservation_intent = new Intent(getApplicationContext(), ReservationDoneActivity.class);
-                startActivity(make_reservation_intent);
+
+                // TODO: get the reservation dates, now use dummy dates
+                RestClient restClient = new RestClient(jwtToken);
+                BookingAPI bookingAPI = restClient.getClient().create(BookingAPI.class);
+                BookingRequest bookingRequest = setBookingRequest();
+
+                bookingAPI.createBooking(bookingRequest)
+                        .enqueue(new Callback<BookingResponse>() {
+                            @Override
+                            public void onResponse(@NonNull retrofit2.Call<BookingResponse> call, @NonNull retrofit2.Response<BookingResponse> response) {
+                                if (response.isSuccessful()) {
+                                    if (response.body() != null) {
+                                        if (response.body().getSuccess()) {
+                                            Toast.makeText(RentRoomPage.this, "Reservation successful", Toast.LENGTH_SHORT).show();
+                                            Intent make_reservation_intent = new Intent(getApplicationContext(), ReservationDoneActivity.class);
+                                            make_reservation_intent.putExtra("user_id", userId);
+                                            make_reservation_intent.putExtra("user_jwt", jwtToken);
+                                            ArrayList<String> roleList = new ArrayList<>();
+                                            for (RoleName role : roles) {
+                                                roleList.add(role.toString());
+                                            }
+                                            make_reservation_intent.putExtra("user_roles", roleList);
+                                            make_reservation_intent.putExtra("apartment_id", apartmentData.getId());
+                                            startActivity(make_reservation_intent);
+                                        } else {
+                                            Toast.makeText(RentRoomPage.this, "1 Reservation failed", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(RentRoomPage.this, "2 Reservation failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(RentRoomPage.this, "3 Reservation failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(@NonNull retrofit2.Call<BookingResponse> call, @NonNull Throwable t) {
+                                Toast.makeText(RentRoomPage.this, "4 Reservation failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
             }
         });
 
         writeReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: go to write review page
                 Toast.makeText(view.getContext(), "Pressed WRITE REVIEW BUTTON", Toast.LENGTH_SHORT).show();
                 Intent write_review_intent = new Intent(getApplicationContext(), WriteReviewActivity.class);
                 startActivity(write_review_intent);
             }
         });
+    }
+
+    private BookingRequest setBookingRequest() {
+        BookingRequest bookingRequest = new BookingRequest();
+        bookingRequest.setApartmentId(apartmentData.getId());
+        bookingRequest.setCheckInDate("2021-05-01");
+        bookingRequest.setCheckOutDate("2021-05-05");
+        return bookingRequest;
     }
 }
