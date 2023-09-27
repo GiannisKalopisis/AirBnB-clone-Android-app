70137a1556ccf90822a92d61b40773288f71bdb3
Giannis Kalopisis <gianniskalopisis@gmail.com>
2023-09-27 22:13:44 +0300

    Added TLS/SSL to Android side
    Changed to https://192.168.1.6:8443/

--- AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/rest/RestClient.java
+++ AndroidAppFront/FakeBnB/app/src/main/java/com/example/fakebnb/rest/RestClient.java
@@ -4,9 +4,16 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyStore;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 public class RestClient {
 
-    private final String BASE_URL = "http://192.168.1.6:8080/";
+    private final String BASE_URL = "https://192.168.1.6:8443/";
     private Retrofit retrofit = null;
     private String authToken = null;
 
@@ -21,6 +28,37 @@ public class RestClient {
 
     private void initializeRetrofit() {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        // Enable TLS support
+        try {
+            // Create a custom TrustManager that trusts all certificates
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustManagerFactory.init((KeyStore) null);
+
+            X509TrustManager trustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new javax.net.ssl.TrustManager[]{trustManager}, null);
+            httpClient.sslSocketFactory(sslContext.getSocketFactory(), trustManager);
+            httpClient.hostnameVerifier((hostname, session) -> true); // Bypass hostname verification (for testing/not for production
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         if (authToken != null) {
             httpClient.addInterceptor(new AuthInterceptor(authToken));
         }
