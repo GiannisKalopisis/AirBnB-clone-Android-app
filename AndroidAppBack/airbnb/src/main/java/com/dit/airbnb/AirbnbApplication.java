791e45bfb156c2b540d242824ac41608fce4c185
geooo109 <geomichas96@gmail.com>
2023-09-27 21:46:13 +0300

    added ssl

--- AndroidAppBack/airbnb/src/main/java/com/dit/airbnb/AirbnbApplication.java
+++ AndroidAppBack/airbnb/src/main/java/com/dit/airbnb/AirbnbApplication.java
@@ -2,11 +2,18 @@ package com.dit.airbnb;
 
 import com.dit.airbnb.service.PopulateDBService;
 import com.dit.airbnb.service.RoleService;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
@@ -33,4 +40,32 @@ public class AirbnbApplication {
         }
     }
 
+    // In case we want to redirect http to https
+    /*
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(true);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+     */
 }
---
+++ AndroidAppBack/airbnb/src/main/java/com/dit/airbnb/service/RecommendationService.java
@@ -0,0 +1,4 @@
+package com.dit.airbnb.service;
+
+public class RecommendationService {
+}
